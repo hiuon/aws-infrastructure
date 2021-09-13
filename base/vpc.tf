47502ab74948f8fbb26060e96c6de8a08d3cf9bb
Viktar Shudzeika <viktar_shudzeika@epam.com>
2021-09-13 17:06:48 +0300

    add private, private db subnets; add igw with rt

--- base/vpc.tf
+++ base/vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = {
-    "Name"      = "cloudx-vpc"
+    "Name"      = "cloudx"
     "Terraform" = "true"
     "Project"   = "cloudx"
   }
@@ -21,8 +21,87 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1${var.aws_zones[count.index]}"
   tags = {
-    "Name"      = "cloudx-subnet-${var.aws_zones[count.index]}"
+    "Name"      = "public_${var.aws_zones[count.index]}"
     "Terraform" = "true"
     "Project"   = "cloudx"
   }
+}
+
+resource "aws_subnet" "private" {
+  provider                = aws.use1
+  count                   = length(var.cidr_blocks_private)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.cidr_blocks_private[count.index]
+  availability_zone       = "us-east-1${var.aws_zones[count.index]}"
+  tags = {
+    "Name"      = "private_${var.aws_zones[count.index]}"
+    "Terraform" = "true"
+    "Project"   = "cloudx"
+  }
+}
+
+resource "aws_subnet" "private_db" {
+  provider                = aws.use1
+  count                   = length(var.cidr_blocks_private_db)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.cidr_blocks_private_db[count.index]
+  availability_zone       = "us-east-1${var.aws_zones[count.index]}"
+  tags = {
+    "Name"      = "private_db_${var.aws_zones[count.index]}"
+    "Terraform" = "true"
+    "Project"   = "cloudx"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  provider = aws.use1
+  vpc_id = aws_vpc.main.id
+  tags = {
+    "Name"      = "cloudx-igw"
+    "Terraform" = "true"
+    "Project"   = "cloudx"
+  }
+}
+
+resource "aws_route_table" "public_subnets" {
+  provider = aws.use1
+  vpc_id = aws_vpc.main.id
+
+   tags = {
+    "Name"      = "public_rt"
+    "Terraform" = "true"
+    "Project"   = "cloudx"
+  }
+}
+
+resource "aws_route" "default_gateway" {
+  provider = aws.use1
+  route_table_id         = aws_route_table.public_subnets.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public_rt" {
+  provider = aws.use1
+  count = length(aws_subnet.public)
+  subnet_id = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_subnets.id
+}
+
+resource "aws_route_table" "private_subnets" {
+  provider = aws.use1
+  vpc_id = aws_vpc.main.id
+
+   tags = {
+    "Name"      = "private_rt"
+    "Terraform" = "true"
+    "Project"   = "cloudx"
+  }
+}
+
+resource "aws_route_table_association" "private_rt" {
+  provider = aws.use1
+  count = length(aws_subnet.private)
+  subnet_id = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_subnets.id
 }
